febda3476a45cc2d0bd88c7112a4f44622e5030f
Asef <50338626+Asef00@users.noreply.github.com>
2022-07-14 13:04:22 -0400

    init

---
+++ pages/index.vue
@@ -0,0 +1,24 @@
+<template>
+  <v-runtime-template :template="template">
+    <Tutorial />
+  </v-runtime-template>
+</template>
+
+<script>
+import VRuntimeTemplate from "v-runtime-template";
+
+export default {
+  name: "IndexPage",
+
+  components: {
+    VRuntimeTemplate,
+  },
+
+  data() {
+    return {
+      name: "Mellow",
+      template: `<span>Hello {{ name }}!</span>`,
+    };
+  },
+};
+</script>
